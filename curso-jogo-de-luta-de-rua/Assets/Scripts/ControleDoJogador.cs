da2f7acf3fd9f528052732b6bde25b8e89f9bbe5
GabrielChiarelli <102618272+GabrielChiarelli@users.noreply.github.com>
2024-01-18 14:13:30 -0300

    Aula 06

--- curso-jogo-de-luta-de-rua/Assets/Scripts/ControleDoJogador.cs
+++ curso-jogo-de-luta-de-rua/Assets/Scripts/ControleDoJogador.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class ControleDoJogador : MonoBehaviour
 {
+    [Header("Referências Gerais")]
     private Rigidbody2D oRigidbody2D;
 
+    [Header("Movimento do Jogador")]
     [SerializeField] private float velocidadeDoJogador;
     private Vector2 inputDeMovimento;
     private Vector2 direcaoDoMovimento;
@@ -18,16 +20,32 @@ public class ControleDoJogador : MonoBehaviour
     private void Update()
     {
         ReceberInputs();
+        EspelharJogador();
         MovimentarJogador();
     }
 
     private void ReceberInputs()
     {
+        // Armazena a direção que o Jogador quer seguir
         inputDeMovimento = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    private void EspelharJogador()
+    {
+        // Faz o Jogador olhar para a direção que está andando (direita / esquerda)
+        if (inputDeMovimento.x == 1)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else if (inputDeMovimento.x == -1)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
     }
 
     private void MovimentarJogador()
     {
+        // Movimentar o Jogador com base na sua direção
         direcaoDoMovimento = inputDeMovimento.normalized;
         oRigidbody2D.velocity = direcaoDoMovimento * velocidadeDoJogador;
     }
