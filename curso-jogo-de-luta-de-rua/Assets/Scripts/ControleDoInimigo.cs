c667e5f5e0e22abe26774e0726bc2132e8a916a7
GabrielChiarelli <102618272+GabrielChiarelli@users.noreply.github.com>
2024-01-22 15:29:28 -0300

    Aula 16

--- curso-jogo-de-luta-de-rua/Assets/Scripts/ControleDoInimigo.cs
+++ curso-jogo-de-luta-de-rua/Assets/Scripts/ControleDoInimigo.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class ControleDoInimigo : MonoBehaviour
 {
+    [Header("Referências Gerais")]
     private Rigidbody2D oRigidbody2D;
     private GameObject oJogador;
 
+    [Header("Movimento do Inimigo")]
     [SerializeField] private float velocidadeDoInimigo;
     private Vector2 direcaoDoMovimento;
 
@@ -18,11 +20,26 @@ public class ControleDoInimigo : MonoBehaviour
 
     private void Update()
     {
+        EspelharInimigo();
         SeguirJogador();
+    }
+
+    private void EspelharInimigo()
+    {
+        // Faz o Inimigo olhar na fireção do Jogador (Esquerda / Direita)
+        if (oJogador.transform.position.x > transform.position.x)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if (oJogador.transform.position.x < transform.position.x)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
     }
 
     private void SeguirJogador()
     {
+        // Armazena a posição do Jogador e se movimenta à ela
         direcaoDoMovimento = (oJogador.transform.position - transform.position).normalized;
         oRigidbody2D.velocity = direcaoDoMovimento * velocidadeDoInimigo;
     }
